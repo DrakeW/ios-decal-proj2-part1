d8931be038edaaebef889e321e15ed8618a67d55
DrakeW <junyuw@berkeley.edu>
2017-03-14 17:22:09 -0700

    added post functionality

---
+++ snapChatProject/FeedTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedTableViewCell.swift
+//  snapChatProject
+//
+//  Created by Junyu Wang on 3/14/17.
+//  Copyright © 2017 org.iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var feedNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
--- snapChatProject/ImagePickerController.swift
+++ snapChatProject/ImagePickerController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ImagePickerController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet var imageCollectionView: UICollectionView!
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageCollectionView.collectionViewLayout = ImageFlowLayout.init()
@@ -24,8 +26,19 @@ class ImagePickerController: UIViewController, UICollectionViewDataSource, UICol
 
     func selectImage(_ image: UIImage) {
         //The image being selected is passed in as "image".
+        selectedImage = image
+        performSegue(withIdentifier: "ImagePickerToFeedViewSegue", sender: self)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "ImagePickerToFeedViewSegue" {
+                if let dest = segue.destination as? FeedViewController {
+                    dest.imageToPost = selectedImage
+                }
+            }
+        }
+    }
     
     
     //DON'T MODIFY CODE HERE AND BELOW!
